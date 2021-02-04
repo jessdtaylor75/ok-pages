39e97a531a4c234775c4eafd9de50d58ed190bd5
aerpass-prime <jess@aerpass.com>
2021-02-04 01:31:37 -0500

    Add Easy-Access to onboard UX flow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -495,6 +495,10 @@ const routes: Routes = [
     path: 'success',
     loadChildren: () => import('./success/success.module').then( m => m.SuccessPageModule)
   },
+  {
+    path: 'easy-access',
+    loadChildren: () => import('./easy-access/easy-access.module').then( m => m.EasyAccessPageModule)
+  },
 ];
 
 @NgModule({
