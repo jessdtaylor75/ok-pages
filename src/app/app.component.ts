c68f41a998f70eab40dd5b4977d1aced264bd3cf
aerpass-prime <jess@aerpass.com>
2022-08-08 20:39:16 -0400

    Remove in-app onboarding flow, replace with connect device flow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,40 @@
 import { Component } from '@angular/core';
-
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+
+//CODE SNIPPET for InlineModal
+//SOURCE: https://ionicframework.com/docs/api/modal
+//import { Component, ViewChild } from '@angular/core';
+//import { IonModal } from '@ionic/angular';
+//import { OverlayEventDetail } from '@ionic/core/components';
+//
+//@Component({
+//  selector: 'app-example',
+//  templateUrl: 'example.component.html',
+//})
+//export class ExampleComponent {
+//  @ViewChild(IonModal) modal: IonModal;
+//
+//  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
+//  name: string;
+//
+//  cancel() {
+//    this.modal.dismiss(null, 'cancel');
+//  }
+//
+//  confirm() {
+//    this.modal.dismiss(this.name, 'confirm');
+//  }
+//
+//  onWillDismiss(event: Event) {
+//    const ev = event as CustomEvent<OverlayEventDetail<string>>;
+//    if (ev.detail.role === 'confirm') {
+//      this.message = `Hello, ${ev.detail.data}!`;
+//    }
+//  }
+//}
+//END CODE SNIPPET
 
 @Component({
   selector: 'app-root',
